34c657a35f1d359520a6d2deefa2ed54d4338bd3
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2025-01-06 21:16:37 +0100

    Cabin data gotten

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,3 +1,11 @@
+import supabase from "./supabase";
+
 export async function getCabins() {
-  hg;
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin can not be loaded");
+  }
+  return data;
 }
