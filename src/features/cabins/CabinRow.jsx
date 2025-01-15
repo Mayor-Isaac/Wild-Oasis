70f58cbac7db643a1cbd5156d658262543be0632
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2025-01-15 16:22:30 +0100

    Menu Toggle Implemented

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,24 +3,26 @@ import { HiSquare2Stack, HiOutlinePencil, HiTrash } from "react-icons/hi2";
 // import { HiOutlinePencil, HiTrash } from "react-icons/hi";
 
 ////////////////////////////
-import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
+import Menus from "../../ui/Menus";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -75,7 +77,7 @@ function CabinRow({ cabin }) {
     console.log("Here");
   }
   return (
-    <TableRow role="row">
+    <Table.Row>
       <Img src={imageUrl} alt={name} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity}</div>
@@ -89,6 +91,7 @@ function CabinRow({ cabin }) {
         <button disabled={isCreating} onClick={() => handleDuplicate}>
           <HiSquare2Stack />
         </button>
+
         <Modal>
           <Modal.Open opens="edit">
             <button>
@@ -98,12 +101,31 @@ function CabinRow({ cabin }) {
           <Modal.Window name="edit">
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
-          <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-            <HiTrash />
-          </button>
+
+          <Modal.Open opens="delete">
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="cabins"
+              disabled={isDeleting}
+              onConfirm={() => deleteCabin(cabinId)}
+            />
+          </Modal.Window>
         </Modal>
+
+        <Menus.Menu>
+          <Menus.Toggle id={cabinId} />
+          <Menus.List id={cabinId}>
+            <Menus.Button>Duplicate</Menus.Button>
+            <Menus.Button>Edit</Menus.Button>
+            <Menus.Button>Delete</Menus.Button>
+          </Menus.List>
+        </Menus.Menu>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 }
 export default CabinRow;
